8ae967e043d556634bca31a17666feb7b0eeac18
lyrahgames <lyrahgames@mailbox.org>
2021-07-01 01:20:27 +0200

    Tweak version test

--- tests/version.cpp
+++ tests/version.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 int main() {
   cout << left << setw(40) << "LYRAHGAMES_GNUPLOT_VERSION"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_VERSION << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_VERSION << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_VERSION_STR"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_VERSION_STR << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_VERSION_STR << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_VERSION_ID"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_VERSION_ID << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_VERSION_ID << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_VERSION_FULL"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_VERSION_FULL << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_VERSION_FULL << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_VERSION_MAJOR"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_VERSION_MAJOR << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_VERSION_MAJOR << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_VERSION_MINOR"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_VERSION_MINOR << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_VERSION_MINOR << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_VERSION_PATCH"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_VERSION_PATCH << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_VERSION_PATCH << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_PRE_RELEASE"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_PRE_RELEASE << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_PRE_RELEASE << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_SNAPSHOT_SN"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_SNAPSHOT_SN << '\n'
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_SNAPSHOT_SN << '\n'
        << left << setw(40) << "LYRAHGAMES_GNUPLOT_SNAPSHOT_ID"
-       << " = " << right << setw(30) << LYRAHGAMES_GNUPLOT_SNAPSHOT_ID << '\n';
+       << " = " << right << setw(40) << LYRAHGAMES_GNUPLOT_SNAPSHOT_ID << '\n';
 }
