8e60c7a9d422af98b569bff41e9661cb221f579f
lyrahgames <lyrahgames@mailbox.org>
2021-07-05 21:56:05 +0200

    Remove manual include guard in main header

--- lyrahgames/gnuplot/gnuplot.hpp
+++ lyrahgames/gnuplot/gnuplot.hpp
@@ -1,11 +1,7 @@
-// #pragma once
-#ifndef LYRAHGAMES_GNUPLOT_GNUPLOT_HPP
-#define LYRAHGAMES_GNUPLOT_GNUPLOT_HPP
+#pragma once
 
 #include <lyrahgames/gnuplot/version.hpp>
 //
 #include <lyrahgames/gnuplot/pipe.hpp>
 //
 #include <lyrahgames/gnuplot/temporary_file.hpp>
-
-#endif  // LYRAHGAMES_GNUPLOT_GNUPLOT_HPP
