dbe87fb00c7cba865d134f65f4f5fde37e9e1c3c
lyrahgames <lyrahgames@mailbox.org>
2021-07-07 02:06:55 +0200

    Fix pipeline test

--- tests/pipe.cpp
+++ tests/pipe.cpp
@@ -1,8 +1,19 @@
+#include <iostream>
+#include <string>
+//
 #include <lyrahgames/gnuplot/pipe.hpp>
 
+using namespace std;
 using namespace lyrahgames;
 
-int main() {
+int main(int argc, char* argv[]) {
+  if ((argc == 2) && (string(argv[1]) == "--mock")) {
+    string line;
+    while (getline(cin, line))
+      cout << line << '\n';
+    return 0;
+  }
+
   gnuplot::pipe plot{};
   plot << "plot sin(x)\n"
        << "replot 0.5*cos(x)\n";
